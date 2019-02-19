5e04b4f4b34a029c956cc1f38706a11af31d14a1
Rafael Batista Arroyo <lokhrafael@hotmail.com>
2019-02-19 19:28:59 -0400

    Add files via upload

---
+++ MVCRUD/MVCRUD/Migrations/20190219205928_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MVCRUD.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    EmpCode = table.Column<string>(type: "varchar(10)", nullable: true),
+                    FullName = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    OfficeLocation = table.Column<string>(type: "varchar(100)", nullable: true),
+                    Position = table.Column<string>(type: "varchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
